90afe9a912c7732129cd87b00d3d525634a28900
AdolphKim <als981209@gmail.com>
2025-01-12 22:15:35 +0900

    Init 2

---
+++ src/components/NameTag.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class ="container">
+        <h1 :value ="name"></h1>
+    </div>
+</template>
+<script>
+    export default{
+        setup(){
+            const name = props.name;
+            return {
+                name,
+            }
+        },
+        props : {
+            name : String,
+        }
+    }
+</script>
